e9e5bee1c3791e2942043501b61b372a7a81f762
jamesj995 <mrjam7@gmail.com>
2021-06-12 13:46:14 -0500

    changed html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var dropDownEl = document.querySelector(".rover_select");
+var dropDownEl = document.querySelector("#roverSelect");
 var radioBtnEl = document.querySelector("#buttonSelect");
 var roverImageEl = document.querySelector("#rover_image");
 
@@ -7,10 +7,18 @@ var formSubmitHandler = function (event) {
 };
 
 var getRoverPhotos = function (rover, date, camera) {
-    var rover = 'curiosity';
-    var date = '2018-2-3'
-    var camera = 'rhaz';
-    var apiUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/' + rover + '/photos' + "?earth_date=" + date + "&camera=" + camera + "&api_key=DEMO_KEY";
+  var rover = "curiosity";
+  var date = "2016-6-3";
+  var camera = "fhaz";
+  var apiUrl =
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/" +
+    rover +
+    "/photos" +
+    "?earth_date=" +
+    date +
+    "&camera=" +
+    camera +
+    "&api_key=DEMO_KEY";
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
@@ -18,26 +26,30 @@ var getRoverPhotos = function (rover, date, camera) {
           displayPhotos(data);
         });
       } else {
-        alert('Error: ' + response.statusText);
+        alert("Error: " + response.statusText);
       }
     })
     .catch(function (error) {
-      alert('Unable to connect to NASA');
+      alert("Unable to connect to NASA");
     });
 };
 
 var displayPhotos = function (api) {
-    if(api.length === 0 ) {
-        return;
-    }
-    var roverImage = api.photos[0].img_src;
+  if (api.length === 0) {
+    return;
+  }
+  var roverImage = api.photos[0].img_src;
 
-    roverImageEl.setAttribute('src', roverImage);
-}
+  roverImageEl.setAttribute("src", roverImage);
+  console.log(roverImage);
+};
 
 var renderMarsWeather = function () {};
 
 dropDownEl.addEventListener("submit", formSubmitHandler);
 radioBtnEl.addEventListener("submit", formSubmitHandler);
 
-getRoverPhotos();
+//getRoverPhotos();
+$(document).ready(function () {
+  $("select").formSelect();
+});
