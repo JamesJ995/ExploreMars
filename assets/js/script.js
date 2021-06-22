39b4f30e6d7c0b39f6f5600afb44ba9249cfd28a
jamesj995 <mrjam7@gmail.com>
2021-06-21 22:33:50 -0500

    merge commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,6 @@ var getRoverPhotos = function () {
         });
       } else {
         alert("Error: " + response.statusText);
-        console.log(alert);
       }
     })
     .catch(function (error) {
